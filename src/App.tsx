98743d4b29138109632b069a1f1ad37c65ca33ed
banuka <banukaramanayaka76@gmail.com>
2024-01-07 10:52:11 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+/*
+ *CONSIDER THE PIXEL DENSITY OF THE DEVICE  . NEED TO FIX FOR DIFFERENT DEVICES
+ */
+
+import SDF from "./components/SDF"; //sdf 01
+
+function App() {
+  return (
+    <>
+      <SDF />
+    </>
+  );
+}
+
+export default App;
